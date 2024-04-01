e2fa8578472f4167a9be130b82ef8d264bce7f5c
Hundredvisionsguy <winikkc@hsd.k12.or.us>
2024-04-01 15:29:51 -0700

    improving spinbox widgets with prefixes and suffixes and more

--- windchill_calculator/app.py
+++ windchill_calculator/app.py
@@ -2,19 +2,14 @@ import sys
 
 from PyQt6.QtWidgets import (
     QApplication,
-    QCheckBox,
     QComboBox,
-    QDateEdit,
-    QDateTimeEdit,
     QDoubleSpinBox,
     QLabel,
     QLineEdit,
     QMainWindow,
     QPushButton,
-    QRadioButton,
-    QSlider,
     QSpinBox,
-    QTimeEdit,
+    QHBoxLayout,
     QVBoxLayout,
     QWidget,
 )
@@ -25,36 +20,46 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Widgets App")
+        self.setWindowTitle("Windchill Calculator")
+        self.setContentsMargins(12, 12, 12, 12)
+        self.resize(320, 240)
 
         layout = QVBoxLayout()
-        widgets = [
-            QCheckBox,
-            QComboBox,
-            QDateEdit,
-            QDateTimeEdit,
-            QDoubleSpinBox,
-            QLabel,
-            QLineEdit,
-            QPushButton,
-            QRadioButton,
-            QSlider,
-            QSpinBox,
-            QTimeEdit,
-        ]
+        title_label = QLabel("Wind Chill Calculator")
 
-        for w in widgets:
-            layout.addWidget(w())
+        # Temperature input
+        temp_layout = QHBoxLayout()
+        temp_label = QLabel("Temperature: ")
+        temp_spinbox = QSpinBox()
+        temp_spinbox.setSuffix(" °F")
+        temp_spinbox.setMaximum(50)
+        temp_layout.addWidget(temp_label)
+        temp_layout.addWidget(temp_spinbox)
+
+        # Money Input
+        money_layout = QHBoxLayout()
+        money_label = QLabel("Money Amount: ")
+        money_spinbox = QDoubleSpinBox()
+        money_spinbox.setPrefix("$ ")
+        money_spinbox.setDecimals(2)
+        money_spinbox.setMinimum(0.0)
+        money_layout.addWidget(money_label)
+        money_layout.addWidget(money_spinbox)
+
+        # add widgets & layouts to main layout
+        layout.addWidget(title_label)
+        layout.addLayout(temp_layout)
+        layout.addLayout(money_layout)
 
         widget = QWidget()
         widget.setLayout(layout)
 
-        # Set the central widget of the Window. Widget will expand
-        # to take up all the space in the window by default.
+        # Set the central widget of the Window. 
         self.setCentralWidget(widget)
 
 
 app = QApplication(sys.argv)
+app.setStyle("Fusion")
 window = MainWindow()
 window.show()
 
